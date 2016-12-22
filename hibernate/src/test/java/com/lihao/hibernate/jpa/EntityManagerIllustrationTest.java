e48e09c2a92f6d07a8dded28630ddf0c89d9c2f9
sbwdlihao <sbwdlihao@gmail.com>
2016-12-22 11:43:43 +0800

    hibernate criteria test

--- hibernate/src/test/java/com/lihao/hibernate/jpa/EntityManagerIllustrationTest.java
+++ hibernate/src/test/java/com/lihao/hibernate/jpa/EntityManagerIllustrationTest.java
@@ -2,13 +2,13 @@ package com.lihao.hibernate.jpa;
 
 import com.lihao.hibernate.JpaTest;
 import com.lihao.hibernate.annotations.Event;
-import org.junit.After;
-import org.junit.Before;
+import com.lihao.hibernate.annotations.Event_;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +30,18 @@ public class EntityManagerIllustrationTest extends JpaTest{
     entityManager.getTransaction().begin();
     List<Event> result = entityManager.createQuery("from Event ", Event.class).getResultList();
     result.forEach(event->System.out.println("Event (" + event.getDate() + ") : " + event.getTitle()));
+    entityManager.getTransaction().commit();
+    entityManager.close();
+
+    entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Event> criteriaQuery = builder.createQuery(Event.class);
+    Root<Event> root = criteriaQuery.from(Event.class);
+    criteriaQuery.select(root);
+    criteriaQuery.where(builder.equal(root.get(Event_.id), 1L));
+    Event event = entityManager.createQuery(criteriaQuery).getSingleResult();
+    System.out.println("Event (" + event.getDate() + ") : " + event.getTitle());
     entityManager.getTransaction().commit();
     entityManager.close();
   }
